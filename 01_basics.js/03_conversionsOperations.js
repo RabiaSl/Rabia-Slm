66ecd871bbaa6528ed0d6f7c74c96c15678183d5
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-04 17:21:19 +0200

    variables

--- 01_basics.js/03_conversionsOperations.js
+++ 01_basics.js/03_conversionsOperations.js
@@ -1,1 +1,28 @@
+let score = 33
+console.log( score);
+let score = "33abc"
+console.log( score);
+console.log( typeof score); 
+console.log(typeof(score)); //there are two ways to write//
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33//
+// "33abc" => NaN//
+// true => 1;
+// false=> 0;
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true; 0 => false;
+//"" => false;
+//"Rabia" => true;
+
+
+
+
+
 
