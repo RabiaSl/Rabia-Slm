2db8c555ebb4ec6e5c7c5745a01b4c841bafeda6
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-04 12:34:29 +0200

    datatypes

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,16 @@
+
+console.log('Rabia');
+const accountId=144553
+let accountEmail="rabia.s@gmail.com"
+varaccountPassword="12345"
+accountCity="Frankfurt"
+// in javascript you can use directly variable value without mentioning var or let in begning but best way is to put let in the begning//
+
+//accoundId = 2 // not allowed
+
+
+accountEmail="123@gmail.com"
+  accountPassword="123456"
+accountCity="Berlin"
+console.log(accountId);
+/* prefer not to use var bacause of issue in block and functionak's scope*//
---
+++ 01_basics.js/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3) // we are using node.js not broweser
+console.log("Rabia");
+let name = "Rabia" // string data type//
+let age = 28 // number's value//
+let isLoggedIn = true/false // boolean
+
+//number range => 2 to power 53
+// bigint when number is big
+// string=> "" can used double and single code but double is better
+//boolean => true/ false
+// null  => standalone value
+//undefined => when value is not defined 
+// symbol=> unique 
+// objects 
+console.log(typeof"Rabia"); //string
+console.log(typeof undeifned);// undefined
+console.log(typeof null); //object
+
+
