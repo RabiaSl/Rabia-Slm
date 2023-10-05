2ae9a2c91eacb360eba1338b7486e4cddc06c883
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-05 10:51:51 +0200

    comparison

--- 01_basics.js/04_comparison.js
+++ 01_basics.js/04_comparison.js
@@ -1,1 +1,18 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+console.log("2" > 1);//comparsion of two different data types
+console.log("02" > 1);
+console.log(null > 0);// false because null is an empty value
+console.log(null == 0);//false
+console.log(null >= 0);//true
+console.log(null < 0);//false
+console.log( undefined == 0);
+console.log( undefined > 0);
+console.log( undefined > 0);// avoid to compare two different datatypes because it can confuse you easily
 
+//===// stricktly check
+
+console.log("2" === 2);
