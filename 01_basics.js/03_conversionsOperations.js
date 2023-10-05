7e606e71bfea8425e6818cbccc6b3e608a688d0e
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-05 10:21:13 +0200

    conversionsoperations

--- 01_basics.js/03_conversionsOperations.js
+++ 01_basics.js/03_conversionsOperations.js
@@ -72,6 +72,12 @@ console.log("");//0 //false
 console.log(+"");//0
 let num1, num2, num3
 num1 = num2 = num3 =2+2
+let num1, num2, num3
+num1 = num2 = num3 =2+2
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);//101
+
 
 
 
