ca52389186af9b56e7af6a7b8c5e9a097ca7bd8a
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-05 11:30:59 +0200

    Update datatype.summary.js

--- 01_basics.js/datatype.summary.js
+++ 01_basics.js/datatype.summary.js
@@ -1,1 +1,18 @@
+// primitive
+// 7types : String, Number, Boolean, Null, Undefined, Symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail; // undeifned//
+let userEmail = undefined
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+console.log(id === anotherId); //false
+const bigNmber = 3343923050092090950n
+
+//Reference (Non primitive)
+//Arrays, Objects, Functions
+
 
