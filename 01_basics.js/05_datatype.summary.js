2ba52221d80456c2d020917254dbdf10e86ca437
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-05 11:49:46 +0200

    Update and rename datatype.summary.js to 05_datatype.summary.js

---
+++ 01_basics.js/05_datatype.summary.js
@@ -0,0 +1,25 @@
+// primitive
+// 7types : String, Number, Boolean, Null, Undefined, Symbol, bigInt
+
+const score = "100" // string // type of= string
+const scoreValue = 100.3 //  number// typeof = number
+const isLoggedIn = false // boolean// typeof = boolean
+const outSideTemp = null// typeof = object
+let userEmail; // undeifned // typeof = undefined
+let userEmail = undefined //
+const id = Symbol ('123') // symbol  // typeof = symbol
+const anotherId = Symbol ('123')
+console.log(id === anotherId); //false
+const bigNmber = 3343923050092090950n // // typeof = undefined
+
+//Reference (Non primitive)
+//Arrays, Objects, Functions
+const heros = [ "superman", "spiderman", " ironman", "batman"]
+ let myObj = {  
+  name: " Maria",
+  age: "33",
+    }
+
+const myFunction = function(){console.log("Hello world");} // function//typeof = function
+
+
