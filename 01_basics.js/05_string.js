c51122b7ba6dd466ea97d4ecdef6f9381272eb98
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-06 04:56:20 +0200

    Update 05_string.js

--- 01_basics.js/05_string.js
+++ 01_basics.js/05_string.js
@@ -1,1 +1,21 @@
+const name = "Rabia" // string declaration
+const repoCount = 50
+//console.log(name + repoCount + "value"); // old version of write strings's syntax
+console.log( "Hello my name is ${name} and my repo count is ${repoCount}); // mordern way of write string's syntax and called string interpolation and even you use method inside it directly
+ const gameName = new String('rabiahc')  
+console.log(gameName[0]);
+console.log(gameName.__proto__ );
+console.log(gameName.toUpperCase());RABIAHC
+console.log(gameName.charArt('2'));//b
+console.log(gameName.indexOf('b')); //2
+const newString = gameName.substring(0,4)
+console.log(newString); // rabi
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);/r/
+const newStringOne = "  rabia  "
 
+
+
+
+
+            
