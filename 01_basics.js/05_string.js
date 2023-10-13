7550be62e77104fc915042db67b4c54946956c78
RabiaSl <68385578+RabiaSl@users.noreply.github.com>
2023-10-13 06:51:44 +0200

    Update 05_string.js

--- 01_basics.js/05_string.js
+++ 01_basics.js/05_string.js
@@ -2,7 +2,8 @@ const name = "Rabia" // string declaration
 const repoCount = 50
 //console.log(name + repoCount + "value"); // old version of write strings's syntax
 console.log( "Hello my name is ${name} and my repo count is ${repoCount}); // mordern way of write string's syntax and called string interpolation and even you use method inside it directly
- const gameName = new String('rabiahc')  
+ const gameName = new String('rabia_hc-com') //
+anotherway of string decalaration
 console.log(gameName[0]);
 console.log(gameName.__proto__ );
 console.log(gameName.toUpperCase());RABIAHC
@@ -13,7 +14,12 @@ console.log(newString); // rabi
 const anotherString = gameName.slice(-8,4)
 console.log(anotherString);/r/
 const newStringOne = "  rabia  "
-
+console.log(newStringOne);//    rabia with spaces
+console.log(newStringOne.trim);// space removed
+const url = "https://rabia.com/rabia%20hania"
+console.log(url.replace('%20' , '-'))
+console.log (url.includes('sundus'))// false
+console.log(gameName.split('-'));
 
 
 
